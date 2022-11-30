659d9efc7b0f24b1b506d34dd849ab3fac300bd8
vamsik1231 <103033434+vamsik1231@users.noreply.github.com>
2022-11-30 21:28:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ def welcome():
     return "Welcome All"
 
 def main():
-  st.title("Subtraction")
+  st.title("Multiplication")
   html_temp = """
   <div style="background-color:tomato;padding:10px">
-  <h2 style="color:white;text-align:center;">Subtraction of 2 numbers using Streamlit</h2>
+  <h2 style="color:white;text-align:center;">Multiplication of 2 numbers using Streamlit</h2>
   </div>
   """
   st.markdown(html_temp,unsafe_allow_html=True)
   num1 = st.number_input("Number 1")
   num2 = st.number_input("Number 2")
-  result = num1 - num2
+  result = num1*num2
   st.success('The output is {}'.format(result))
   if st.button("Made By"):
-      st.text("Parth Bansal")
-      st.text("21f3000805")
+      st.text("Krishna Vamsi K")
+      st.text("21f3001231")
 
 if __name__=='__main__':
   main()
